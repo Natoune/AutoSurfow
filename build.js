9bb46068c8ab72ebabc0a7a335067c1d1f9f40ec
Natoune <natoune.chd@gmail.com>
2023-03-20 13:48:25 +0100

    Update build.js

--- build.js
+++ build.js
@@ -6,14 +6,98 @@ const { execSync } = require('child_process');
 const { compile } = require('nexe');
 const iconPath = resolve(__dirname, 'icon.ico');
 
+const prettyPlatform = {
+    win32: 'windows',
+    windows: 'windows',
+    win: 'windows',
+    darwin: 'mac',
+    macos: 'mac',
+    mac: 'mac',
+    linux: 'linux',
+    static: 'alpine',
+    alpine: 'alpine',
+}
+
+const prettyArch = {
+    x86: 'x86',
+    arm6: 'arm',
+    arm64: 'arm64',
+    arm6l: 'arm',
+    arm: 'arm',
+    arm7: 'arm',
+    arm7l: 'arm',
+    amd64: 'x64',
+    ia32: 'x86',
+    x32: 'x86',
+    x64: 'x64',
+}
+
+function isVersion(x) {
+    if (!x) {
+        return false;
+    }
+    return /^[\d]+$/.test(x.replace(/v|\.|\s+/g, ''));
+}
+
+function isPlatform(x) {
+    return x in prettyPlatform;
+}
+
+function isArch(x) {
+    return x in prettyArch;
+}
+
+let os = process.platform;
+if (os == 'win32') {
+    os = 'windows';
+} else if (os == 'darwin') {
+    os = 'mac';
+}
+if (process.argv[2] && process.argv[2] != os) {
+    os = process.argv[2];
+}
+
+if (!isPlatform(os)) {
+    console.log('OS not supported');
+    console.log('Supported OS: windows, mac, alpine, linux');
+    process.exit();
+}
+
+let arch = process.arch;
+if (arch == 'x32') {
+    arch = 'x86';
+}
+if (process.argv[3] && process.argv[3] != arch) {
+    arch = process.argv[3];
+}
+
+if (!isArch(arch)) {
+    console.log('Architecture not supported');
+    console.log('Supported architectures: x86, x64, arm, arm64');
+    process.exit();
+}
+
+let nodeVersion = '16.15.0';
+if (process.argv[4] && process.argv[4] != nodeVersion) {
+    nodeVersion = process.argv[4];
+}
+
+if (!isVersion(nodeVersion)) {
+    console.log('Node version not supported');
+    process.exit();
+}
+
+let target = os + '-' + arch + '-' + nodeVersion;
+let fileName = 'auto-surfow-' + target + (os == 'windows' ? '.exe' : '');
+
 const rc = {
     CompanyName: 'Natoune',
     ProductName: 'Auto Surfow',
     FileDescription: description,
     FileVersion: version,
     ProductVersion: version,
-    OriginalFilename: 'auto-surfow-win64.exe',
-    InternalName: 'auto-surfow-win64',
+    OriginalFilename: fileName,
+    InternalName: (fileName.split('.')[0] + '.exe').replace(/-/g, '_' + ' '),
     LegalCopyright: 'Copyright © 2023 Natoune. MIT License.',
 }
 
@@ -27,8 +111,10 @@ async function exists(filename) {
 (async function () {
     await compile({
         input: resolve(__dirname, '../index.js'),
-        output: resolve(__dirname, 'build', 'auto-surfow-win64.exe'),
-        targets: ['windows-x64-16.15.0'],
+        output: resolve(__dirname, 'build', fileName),
+        targets: [
+            target
+        ],
         name: 'Auto Surfow',
         build: true,
         ico: iconPath,
